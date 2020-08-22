894fac0d2d2cad6db60e35eac38ea83c368dd2db
tpopl01 <56597428+tpopl01@users.noreply.github.com>
2020-08-22 21:39:54 +0100

    Added static classes

---
+++ Third_Year_Project_Unity/Assets/Scripts/Actual/Statics/StaticCalculations.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class that hosts any calculation that are likely to be used multiple times
+public static class StaticCalculations
+{
+  
+
+    public static float GetAngle(Vector3 targetPos, Vector3 currentPos, Vector3 currentForward)
+    {
+        Vector3 dir = GetDirection(targetPos, currentPos);
+        return Vector3.Angle(currentForward, dir);
+    }
+
+    public static Vector3 GetDirection(Vector3 targetPos, Vector3 currentPos)
+    {
+        Vector3 directionToLookTo = targetPos - currentPos;
+        directionToLookTo.y = 0;
+
+        return directionToLookTo;
+    }
+
+    public static Quaternion GetLookRotation(Vector3 targetPos, Vector3 currentPos, Vector3 currentForward, out bool shouldRotate, float threshold = 0.1f)
+    {
+        shouldRotate = false;
+        Vector3 dir = GetDirection(targetPos, currentPos);
+        float angle = Vector3.Angle(currentForward, dir);
+        if (angle > threshold)
+        {
+            shouldRotate = true;
+            return Quaternion.LookRotation(dir);
+        }
+
+        return Quaternion.identity;
+    }
+
+    public static string ProcessObjectName(string name)
+    {
+        if (name.Length > 7)
+            name = name.Remove(name.Length - 7);
+        return name;
+    }
+}
---
+++ Third_Year_Project_Unity/Assets/Scripts/Actual/Statics/StaticLevel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class StaticLevel
+{
+    #region Level
+    public static string username;
+    public static string levelName;
+    public static string levelTitle;
+    public static string objectiveTitle;
+    #endregion
+
+    #region Settings
+    public static float maxSpeed = 5;
+    public static bool enableQuestMarkers = true;
+
+    #region Hands
+    public static bool noHands = true;
+    public static bool noStick = true;
+    public static bool leftHand = false;
+    #endregion
+
+    #region Comfort
+    public static bool acceleration = true;
+    public static bool smoothMovement = false;
+    public static float tunnelVisionAmount = 0.75f;
+    public static bool accelerationTunnelVision = false;
+    public static bool turningTunnelVision = true;
+    public static int movementStepAmount = 0;
+    public static int turnStepAmount = 0;
+    #endregion
+
+    #endregion
+}
