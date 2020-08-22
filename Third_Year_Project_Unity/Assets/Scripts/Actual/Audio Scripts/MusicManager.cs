ab2f45c4b101c01b5b97a5ee9dffd5b8e0361589
tpopl01 <56597428+tpopl01@users.noreply.github.com>
2020-08-22 21:51:54 +0100

    Added Audio Handling Scripts

---
+++ Third_Year_Project_Unity/Assets/Scripts/Actual/Audio Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace tpopl001.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class MusicManager : MonoBehaviour
+    {
+        private AudioSource aS;
+        private AudioClip[] clips = null;
+
+        //initialise the variables
+        void Start()
+        {
+            aS = GetComponent<AudioSource>();
+            clips = Resources.LoadAll<AudioClip>("Audio/Music/");
+        }
+
+        //try to play a random music clip
+        void Update()
+        {
+            PlayClip(clips[Random.Range(0, clips.Length)]);
+        }
+
+        /// <summary>
+        /// Play an audio clip if one is not already playing
+        /// </summary>
+        /// /// <param name="clip">The clip to play</param>
+        void PlayClip(AudioClip clip)
+        {
+            if (!aS.isPlaying)
+            {
+                aS.PlayOneShot(clip);
+            }
+        }
+    }
+}
